4edb24fb4640f39a0e830e5770b06d38ea4d4dbd
Adam Freedman <adamfreedman@fas.harvard.edu>
2025-11-14 09:39:30 -0500

    first commits for emptydrops rule

---
+++ workflow/rules/emptydrops.smk
@@ -0,0 +1,16 @@
+def input_function(wildcards):
+    tenx_dir = sampleinfo.loc[sampleinfo["sampleid"] == wildcards.sample, "tenx_datadir"].values[0]
+    input_path = os.path.join(tenx_dir, "raw_feature_bc_matrix")
+    return input_path
+
+rule emptydrops:
+    input:
+        input_function
+    output:
+         "results/emptydrops/seurat_emptydrops_{sample}.rds"
+    conda:
+        "../envs/emptydrops.yml"
+    shell:
+        """
+        Rscript workflow/scripts/emptydrops.R  {input} {output}
+        """
---
+++ workflow/scripts/emptydrops.R
@@ -0,0 +1,18 @@
+args <- commandArgs(trailingOnly = TRUE)
+raw <- args[1]
+output <- args[2]
+
+library("Seurat")
+library("DropletUtils")
+
+droputil_rawdata <-read10xCounts(raw)
+colnames(droputil_rawdata) <- droputil_rawdata$Barcode
+rownames(droputil_rawdata) <- make.unique(rowData(droputil_rawdata)$Symbol)
+dropletutils_out <-emptyDrops(counts(droputil_rawdata))
+is.cell <-!is.na(dropletutils_out$FDR) & dropletutils_out$FDR<0.01
+cell_barcodes <- colnames(droputil_rawdata)[which(is.cell)]
+droputil_filtered <- droputil_rawdata[,is.cell]
+droputil_filtered <- logNormCounts(droputil_filtered)
+seurat_droputil_filtered <- as.Seurat(droputil_filtered, counts = "counts",data="logcounts") 
+seurat_droputil_filtered[["percent.mt"]] <- PercentageFeatureSet(seurat_droputil_filtered, pattern = "(?i)^mt-")
+saveRDS(seurat_droputils_filtered,file=output)
